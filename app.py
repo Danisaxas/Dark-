c009288b5469fbb7f6132feb3221ebd0e2282eea
Danisaxas <danluismay@gmail.com>
2025-02-27 17:42:21 -0400

    Add requirements.txt

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,7 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Obtiene el puerto de la variable de entorno, si no está disponible usa el puerto 5000
+    port = int(os.environ.get("PORT", 5000))  
+    # Ejecuta la aplicación en el puerto y host correcto para Heroku
+    app.run(debug=True, host='0.0.0.0', port=port)
