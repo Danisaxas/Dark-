686a8c4c6e1309ead0033808fe1c140a5545d89c
Danisaxas <danluismay@gmail.com>
2025-02-27 17:46:35 -0400

    Añadir dependencias requeridas

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-import os
 
 app = Flask(__name__)
 
@@ -8,7 +7,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    # Obtiene el puerto de la variable de entorno, si no está disponible usa el puerto 5000
-    port = int(os.environ.get("PORT", 5000))  
-    # Ejecuta la aplicación en el puerto y host correcto para Heroku
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True)
