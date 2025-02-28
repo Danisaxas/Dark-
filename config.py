d984f3296eddaff585646875167b8b0688015e33
Danisaxas <danluismay@gmail.com>
2025-02-28 09:00:54 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,4 @@
+MONGO_URI = "mongodb://localhost:27017/chatbot_db"
+SECRET_KEY = "Dani1906"
 OPENAI_API_KEY = "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"
+
